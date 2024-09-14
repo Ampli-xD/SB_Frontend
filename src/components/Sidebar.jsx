fd74de01866d1fb1e314df252b3462207ab9e957
Ampli-xD <dishantkapoor29@gmail.com>
2024-09-14 11:03:06 +0530

    made changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-  import React, { useState, useEffect } from 'react';
+  import React, { useState, useEffect, useRef } from 'react';
   import { Users, Database, Download, Menu } from 'lucide-react';
   import io from 'socket.io-client';
   
